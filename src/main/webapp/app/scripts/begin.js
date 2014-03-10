5c4f08b07b598fb1905f99d78c9663e8ccf04ade
Rick Li <racke1983cn@gmail.com>
2014-03-10 21:48:39 +0800

    add title

--- src/main/webapp/app/scripts/begin.js
+++ src/main/webapp/app/scripts/begin.js
@@ -1,7 +1,8 @@
 define(function(require, exports) {
-	
-	exports.render = function(tpl, quiz) {
-		var h = tpl(quiz)	
+
+    exports.render = function(tpl, quiz) {
+        var h = tpl(quiz);
+        $('.header-title').text(quiz.name);
         $('#content').html(h);
     };
 });
